3f53d50a27929779cae56829cee809f821ee659f
Souptik Koley <souptikmemari264@gmail.com>
2026-02-23 23:17:21 +0530

    pairarray

--- Java/Array/pairarraysum.java
+++ Java/Array/pairarraysum.java
@@ -2,18 +2,18 @@ public class pairarraysum {
     public static void PairArraySum(int num[]){
         int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
-        for(int i=0;i<num.length;i++){
+        for(int i=0;i<num.length;i++){          // another process : for(int i=0;i<num.length-1;i++){}
             int value=num[i];
             int sum=0;
-            boolean a=false;
+            boolean a=false;                     // then boolean a=false is not necessary 
             for(int j=i+1;j<num.length;j++){
                 System.out.print("("+value+","+num[j]+")");
                 sum=sum+num[j]+value;
-                a=true;
+                a=true;                          // no need
             }
             if(a){
                 System.out.println();
-                System.out.println("The sum is :"+sum);
+                System.out.println("The sum is :"+sum);    // not necessary
             if(sum>max){
                 max=sum;
             }else if(sum<min){
