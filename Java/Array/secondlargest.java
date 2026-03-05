b73a0564937908b08925b4a99b71677b46430d18
Souptik Koley <souptikmemari264@gmail.com>
2026-03-05 16:09:23 +0530

    changes

--- Java/Array/secondlargest.java
+++ Java/Array/secondlargest.java
@@ -1,23 +1,23 @@
 // find second largest element in the array
 public class secondlargest {
-    public static int Secondlargest(int arr[]){
-        int largest=Integer.MIN_VALUE;
-        int secondlargest=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>largest){
-                secondlargest=largest;
-                largest=arr[i];
-            }else if(largest>arr[i] && secondlargest<arr[i]){
-                secondlargest=arr[i];
-            }
+   public static int SecondLargestNumber(int arr[]){
+    int largest=Integer.MIN_VALUE;
+    int secondlargest=Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]>largest){
+            secondlargest=largest;
+            largest=arr[i];
+        }else if(arr[i]>secondlargest && secondlargest!=largest){
+            secondlargest=arr[i];
         }
-        if(secondlargest==Integer.MIN_VALUE){
-            return secondlargest;
-        }
-        return secondlargest;
     }
+    if(secondlargest==Integer.MIN_VALUE){
+        return -1;
+    }
+    return secondlargest;
+   }
  public static void main(String args[]){
         int arr[]={5,10,4,8,11};
-        System.out.println("The largest number is :"+Secondlargest(arr));
+        System.out.println("The largest number is :"+SecondLargestNumber(arr));
  }   
 }
--- Java/Array/thirdlargest.java
+++ Java/Array/thirdlargest.java
@@ -1,28 +1,29 @@
 public class thirdlargest {
     
     // find thirdlargest in the array
-    public static int Thirdlargest(int arr[]){
-        int largest=Integer.MIN_VALUE;
-        int secondlargest=Integer.MIN_VALUE;
-        int thirdlargest=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>largest){
-                thirdlargest=secondlargest;
-                secondlargest=largest;
-                largest=arr[i];
-            }else if(largest>arr[i] && secondlargest<arr[i]){   // find secondlargest
-                thirdlargest=secondlargest;
-                secondlargest=arr[i];
-            }else if(secondlargest>arr[i] && thirdlargest<arr[i]){    // find thirdlargest
-                thirdlargest=arr[i];
-            }
-        }if(thirdlargest==Integer.MIN_VALUE){
-            return thirdlargest;
+   public static int ThirdLargestNumber(int arr[]){
+    int largest=Integer.MIN_VALUE;
+    int secondlargest=Integer.MIN_VALUE;
+    int thirdlargest=Integer.MIN_VALUE;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]>largest){
+            thirdlargest=secondlargest;
+            secondlargest=largest;
+            largest=arr[i];
+        }else if(arr[i]>secondlargest && secondlargest!=largest){
+            thirdlargest=secondlargest;
+            secondlargest=arr[i];
+        }else if(arr[i]>thirdlargest && thirdlargest!=secondlargest){
+            thirdlargest=arr[i];
         }
-        return thirdlargest;
     }
+    if(thirdlargest==Integer.MIN_VALUE){
+        return -1;
+    }
+    return thirdlargest;
+   }
     public static void main(String args[]){
         int arr[]={7,2,8,10,20,5,6,14};
-        System.out.println("The third largest number is :"+Thirdlargest(arr));
+        System.out.println("The third largest number is :"+ThirdLargestNumber(arr));
     }
 }
