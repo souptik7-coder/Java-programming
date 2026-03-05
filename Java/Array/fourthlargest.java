d8da1e6a1fc3ff3276d838e0a3333d72f0fb9005
Souptik Koley <souptikmemari264@gmail.com>
2026-03-05 19:02:22 +0530

    largest number in the array

--- Java/Array/fourthlargest.java
+++ Java/Array/fourthlargest.java
@@ -1,35 +1,33 @@
-public class Fourthlargest {
-
-    // find fourth largest element in th array
+public class Fourthlargest{
     public static int FourthLargestNumber(int arr[]){
-       int largest=Integer.MIN_VALUE;
-       int secondlargest=Integer.MIN_VALUE;
-       int thirdlargest=Integer.MIN_VALUE;
-       int fourthlargest=Integer.MIN_VALUE;
-       for(int i=0;i<arr.length;i++){
-        if(arr[i]>largest){
-            fourthlargest=thirdlargest;
-            thirdlargest=secondlargest;
-            secondlargest=largest;
-            largest=arr[i];
-        }else if(arr[i]>secondlargest && secondlargest!=largest){
-            fourthlargest=thirdlargest;
-            thirdlargest=secondlargest;
-            secondlargest=arr[i];
-        }else if(arr[i]>thirdlargest && thirdlargest!=secondlargest){
-            fourthlargest=thirdlargest;
-            thirdlargest=arr[i];
-        }else if(arr[i]>fourthlargest && fourthlargest!=thirdlargest){
-            fourthlargest=arr[i];
+        int largest=Integer.MIN_VALUE;
+          int secondlargest=Integer.MIN_VALUE;
+            int thirdlargest=Integer.MIN_VALUE;
+              int fourthlargest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest){
+                fourthlargest=thirdlargest;
+                thirdlargest=secondlargest;
+                secondlargest=largest;
+                largest=arr[i];
+            }else if(arr[i]>secondlargest && secondlargest!=largest){
+                fourthlargest=thirdlargest;
+                thirdlargest=secondlargest;
+                secondlargest=arr[i];
+            }else if(arr[i]>thirdlargest && thirdlargest!=secondlargest){
+                fourthlargest=thirdlargest;
+                thirdlargest=arr[i];
+            }else if(arr[i]>fourthlargest && fourthlargest!=thirdlargest){
+                fourthlargest=arr[i];
+            }
         }
-       }
-       if(fourthlargest==Integer.MIN_VALUE){
-        return -1;
-       }
-       return fourthlargest;
+        if(fourthlargest==Integer.MIN_VALUE){
+            return -1;
+        }
+        return fourthlargest;
     }
     public static void main(String args[]){
-        int arr[]={22,4,65,24,10,54,14};
-        System.out.println("The fourthlargest number is :"+FourthLargestNumber(arr));
-    }   
+        int arr[]={50,40,30,20,10};
+        System.out.println("The fourth largest number is :"+FourthLargestNumber(arr));
+    }
 }
