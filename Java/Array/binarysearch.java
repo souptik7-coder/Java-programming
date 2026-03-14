6834009a57f307f720ac74f671dbfd6630298330
Souptik Koley <souptikmemari264@gmail.com>
2026-03-14 17:34:37 +0530

    added

--- Java/Array/binarysearch.java
+++ Java/Array/binarysearch.java
@@ -1,27 +1,30 @@
 public class binarysearch {
-    public static int BinarySearch(int num[],int search_key){
+
+    public static int BinarySearch(int arr[],int key){
         int start=0;
-        int end=num.length-1;
+        int end=arr.length-1;
+        int ans=-1;
         while(start<=end){
             int mid=(start+end)/2;
-            if(num[mid]>search_key){
+            if(arr[mid]==key){
+                ans=mid;
+                end=mid-1;  // search for left 
+            }else if(arr[mid]>key){
                 end=mid-1;
-            }else if(num[mid]<search_key){
+            }else if(arr[mid]<key){
                 start=mid+1;
-            }else if(num[mid]==search_key){
-                return mid;
             }
         }
-        return -1;
+        return ans;
     }
     public static void main(String args[]){
-        int num[]={2,4,6,8,10,12};
-        int search_key=8;
-        int ans=BinarySearch(num, search_key);
+        int arr[]={2,4,6,6,8,10};
+        int key=6; 
+        int ans=BinarySearch(arr, key);
         if(ans==-1){
-            System.out.println("The given index is not found");
-        }else{
-            System.out.println("The given index is : "+ans);
+            System.out.println("The element is not found");
+        }   else {
+            System.out.println("The element is found at "+ans+"th position");
         }
     }
 }
