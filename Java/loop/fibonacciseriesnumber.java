4258803e483ab132faef5a1e140ba4e15dc281ba
Souptik Koley <souptikmemari264@gmail.com>
2026-04-02 01:19:56 +0530

    loop

--- Java/loop/fibonacciseriesnumber.java
+++ Java/loop/fibonacciseriesnumber.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Fibonacci {
+public class fibonacciseriesnumber {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int a=0;
