04b5d18edf451e53fbc01cd9742b2ecbb9a591e2
Souptik Koley <souptikmemari264@gmail.com>
2026-04-02 01:13:50 +0530

    loop

---
+++ Java/loop/Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Fibonacci {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=0;
+        int b=1;
+        System.out.println(a);
+        System.out.println(b);
+        int n=10;
+        for(int i=0;i<n;i++){
+            int next=a+b;
+            System.out.println(next);
+            a=b;
+            b=next;
+        }
+    }
+}
