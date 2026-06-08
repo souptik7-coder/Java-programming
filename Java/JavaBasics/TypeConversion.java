f56657eafb5fe8aa1a6c23ca49e7c1baf1c7ba63
Souptik Koley <souptikmemari264@gmail.com>
2026-06-08 06:44:47 +0530

    added

--- Java/JavaBasics/TypeConversion.java
+++ Java/JavaBasics/TypeConversion.java
@@ -2,6 +2,10 @@ import java.util.*;
 public class TypeConversion{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+       
+       
+        //  Type conversion
+
 
         int a= 50;
         double b = a;
@@ -10,5 +14,23 @@ public class TypeConversion{
         float d = c;
         System.out.println(b);
         System.out.println(d);
+
+
+        // Type Casting
+
+        float a1 = 25.12f;
+        int b1 = (int)a1;
+
+
+        float c1 = 2.5f;
+        int d1 = (int)c1;
+
+            System.out.println(b1);
+
+            System.out.println(d1);
+
+        
+
+
     }
 }
