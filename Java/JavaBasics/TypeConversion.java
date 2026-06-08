5df12336897674b5b0c5da0a524102cedb7ac628
Souptik Koley <souptikmemari264@gmail.com>
2026-06-08 06:32:50 +0530

    added

---
+++ Java/JavaBasics/TypeConversion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TypeConversion{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int a= 50;
+        double b = a;
+
+        int c = 40;
+        float d = c;
+        System.out.println(b);
+        System.out.println(d);
+    }
+}
