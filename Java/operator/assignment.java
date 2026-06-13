546b38b2b64ca12a539eff470ff9716bb25b1ef2
Souptik Koley <souptikmemari264@gmail.com>
2026-06-13 21:04:57 +0530

    operator

--- Java/operator/assignment.java
+++ Java/operator/assignment.java
@@ -2,12 +2,12 @@ import java.util.*;
 public class assignment {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int a=5;
-        int b=10;
-        System.out.println(a=b);
-        System.out.println(a+=b);
-        System.out.println(a-=b);
-        System.out.println(a*=b);
-        System.out.println(a/=b);
+        int a = 5;
+        int b = 10;
+        System.out.println( a = b );  // a = 10
+        System.out.println( a += b );  // a = a + b = 20
+        System.out.println( a -= b );  // a = a - b = 10
+        System.out.println( a *= b );  //  a = a * b = 100
+        System.out.println( a /= b );  // a = a / b  = 10  
     }
 }
