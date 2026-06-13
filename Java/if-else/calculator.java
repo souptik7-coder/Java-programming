ac67c27449966aa41ea209ea89f8974106a03317
Souptik Koley <souptikmemari264@gmail.com>
2026-06-13 23:24:41 +0530

    calculator

--- Java/if-else/calculator.java
+++ Java/if-else/calculator.java
@@ -1,31 +1,42 @@
 import java.util.*;
 public class calculator {
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter your number:");
-        int a=sc.nextInt();
-        System.out.print("Enter your number:");
-        int b=sc.nextInt();
-        System.out.print("Enter your operator:");
-        char operator=sc.next().charAt(0);
-        switch(operator){
-            case '+':
-                System.out.println("The Sum is:"+(a+b));
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first operand : ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter the second operand : ");
+        int b = sc.nextInt();
+
+        System.out.println("Enter the operator : ");
+        int op = sc.next().charAt(0);
+
+        switch(op) {
+            case '+' :
+                System.out.println( "The sum is : " + (a + b) );
+                    break;
+            
+            case '-' :
+                System.out.println( "The sub is :" + (a - b) );
                 break;
-            case '-':
-                System.out.println("The sub is :"+ (a-b));
+            
+            case '*' :
+                System.out.println( "The multiplication is :" + (a * b) );
                 break;
-            case '*':
-                System.out.println("The multiplication is :"+(a*b));
+
+            case '/' :
+                System.out.println( "The divison is :" + (a / b) );
                 break;
-            case '/':
-                System.out.println("The division is :"+(a/b));
+
+            case '%' :
+                System.out.println( "The modulo is :" +  (a % b) );
                 break;
-            case '%':
-                System.out.println("The modulo is:"+(a%b));
-                break;
-            default:
-                System.out.println("error");
+
+            default :
+            System.out.println( "error");
+
         }
+
     }
 }
