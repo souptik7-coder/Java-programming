0f6033235eef15ff96cb19682f49f8d6d4b57fde
Souptik Koley <souptikmemari264@gmail.com>
2026-06-13 22:56:30 +0530

    leap year

--- Java/if-else/leapyear.java
+++ Java/if-else/leapyear.java
@@ -1,14 +1,19 @@
 import java.util.*;
 public class leapyear {
+    
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the year:");
-        int year=sc.nextInt();
-            if((year%4==0 && year%100!=0) && (year%400==0)){
-                System.out.println("The year is leap year");
-            }else{
-                System.out.println("The year is not leap year");
-            }
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter your year :");
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+            System.out.println("The year is leap year");
+        } else {
+            System.out.println("The year is not leap year");
         }
+    }
+
     }
 
