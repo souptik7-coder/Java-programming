1906fc117987d335f0ae6986fe5e7ff4fbf24493
Souptik Koley <souptikmemari264@gmail.com>
2026-06-15 19:57:13 +0530

    sum function

--- Java/Function/sum.java
+++ Java/Function/sum.java
@@ -1,16 +1,21 @@
-// using parameter
 import java.util.*;
 public class sum {
-    public static void Sum(int a,int b){
-        int c=a+b;
-    }
-  public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter first number:");
-        int a=sc.nextInt();
-        System.out.print("Enter second number:");
-        int b=sc.nextInt();
-        int sum=a+b;
-        System.out.println("The sum is :"+sum);
-  }  
+  
+  public static void Sum(int a, int b) {
+
+    int c = a + b;
+    System.out.println("The sum is :" + c);
+  }
+  
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter your number a :");
+    int a = sc.nextInt();
+    System.out.println("Enter your number b :");
+    int b = sc.nextInt();
+
+
+    Sum(a, b);
+  }
 }
